4c8f6ac49b112ddb9c8114c1849283a88079b6dd
ViktorMorgachev <morgachev.v.s@mail.com>
2018-10-21 15:32:49 +0600

    Немного настроил сервер, нужно разобраться с сокетами, чтобы открывался доступный сокет, для сервера, и после можно писать класс для работы с клиентскими сокетами, общения и взаимодействия

--- app/src/main/java/ru/chat/irokez/myapplication/logic/ChatService.java
+++ app/src/main/java/ru/chat/irokez/myapplication/logic/ChatService.java
@@ -10,6 +10,9 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 import trikita.log.Log;
 
@@ -18,9 +21,11 @@ public class ChatService extends Service {
 
     private ServerSocket mSocketListener;
     // если сервер работает при удачном открытом порте
-    private final int serverPort = (int) (Math.random() * 1000);
+    private int serverPort;
     private BufferedReader mBufferedReader;
     private BufferedWriter mBufferedWriter;
+    // Количество открытых соединений (static если служба пересоздастся, то пул соединений сохранится)
+    private static List<ClientConnection> mClientConnections;
 
 
     @Nullable
@@ -30,29 +35,67 @@ public class ChatService extends Service {
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Toast.makeText(this, "Служба запущена:" + serverPort,
-                Toast.LENGTH_SHORT).show();
-        try {
-            mSocketListener = new ServerSocket(serverPort);
-            Log.e("Порт сервера открыт: " + serverPort);
-            while (true){
+    public void onCreate() {
 
+        super.onCreate();
+        if (mClientConnections == null) {
+            mClientConnections = new ArrayList<>();
+        }
+/*
+        try {
+            serverPort = (int) (Math.random() * 1000);
+            for (int i = serverPort; i < 50000; i++) {
+                if (isAvailablePort(i))
+                    break;
+            }
+            mSocketListener = new ServerSocket(serverPort);
+            Log.d("Порт сервера открыт: " + serverPort);
+        } catch (IOException e) {
+            Log.e("Ошибка открытия прослушаемого порта:" + serverPort);
+            e.printStackTrace();
+
+        }*/
+    }
+
+
+    private static boolean isAvailablePort(int port) {
+        try (ServerSocket ignored = new ServerSocket(port)) {
+            return false;
+        } catch (IOException ignored) {
+            return true;
+        }
+    }
+
+    // Допустим пока что порт всегда открывается удачно
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+
+        try {
+            mSocketListener = new ServerSocket((int) (Math.random() * 1000));
+            Toast.makeText(this, "Служба запущена:" + serverPort,
+                    Toast.LENGTH_SHORT).show();
+
+            while (true) {
+                Socket socketClient = mSocketListener.accept();
+                ClientConnection clientConnection = new ClientConnection(socketClient, getApplicationContext());
+                mClientConnections.add(clientConnection);
             }
         } catch (IOException e) {
-            Log.e("Ошибка открытия прослушаемого порта");
             e.printStackTrace();
+            Log.e("Ошибка прослушивания входящих соединений");
         }
-
         return Service.START_NOT_STICKY;
+
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+
         try {
             if (mSocketListener != null)
-            mSocketListener.close();
+                mSocketListener.close();
         } catch (IOException e) {
             Log.e("Ошибка закрытия прослушаемого порта");
             e.printStackTrace();
--- app/src/main/java/ru/chat/irokez/myapplication/logic/ClientConnection.java
+++ app/src/main/java/ru/chat/irokez/myapplication/logic/ClientConnection.java
@@ -1,13 +1,23 @@
 package ru.chat.irokez.myapplication.logic;
 
+import android.content.Context;
+import android.widget.Toast;
+
 import java.net.Socket;
 
 public class ClientConnection extends Thread {
 
     private Socket mSocket;
+    private Context mContext;
+
+    ClientConnection(Socket socket, Context ctx) {
+        mSocket = socket;
+        mContext = ctx;
+    }
 
     @Override
     public void run() {
+        Toast.makeText(mContext, "Новое соединение созданно", Toast.LENGTH_LONG).show();
         super.run();
     }
 }
